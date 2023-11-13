7808e397308153120061654d278f31874fcf6182
GenIl <topgear09@yandex.ru>
2023-11-13 21:55:59 +0300

    feat: update invoice page feat: add delete logic for inv feat: add new server actions

---
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -0,0 +1,42 @@
+import { fetchCustomers, fetchInvoiceById } from '@/app/lib/data';
+import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
+import EditInvoiceForm from '@/app/ui/invoices/edit-form';
+import { redirect } from 'next/navigation';
+import { FC } from 'react';
+
+interface UpdateInvoiceProps {
+  params: {
+    id: string;
+  };
+}
+
+const UpdateInvoice: FC<UpdateInvoiceProps> = async ({ params: { id } }) => {
+  //получить данные о счете
+  //получить данные о пользователях
+  const [invoice, customers] = await Promise.all([
+    fetchInvoiceById(id),
+    fetchCustomers(),
+  ]);
+  //проверить наличие данных
+  if (!invoice) {
+    redirect('/dashboard/invoices');
+  }
+
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: 'Invoices', href: '/dashboard/invoices' },
+          {
+            label: 'Edit Invoice',
+            href: `/dashboard/invoices/${id}/edit`,
+            active: true,
+          },
+        ]}
+      />
+      <EditInvoiceForm invoice={invoice} customers={customers} />
+    </main>
+  );
+};
+
+export default UpdateInvoice;
--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -8,29 +8,54 @@ import { redirect } from 'next/navigation';
 const InvoiceSchema = z.object({
   id: z.string(),
   customerId: z.string(),
-  amount: z.coerce.number(),//изменить тип данных на число и проверить его
+  amount: z.coerce.number(), //изменить тип данных на число и проверить его
   status: z.enum(['pending', 'paid']),
   date: z.string(),
-})
+});
 
 //указать что опускаем два поля из схемы
-const CreateInvoice = InvoiceSchema.omit({ id: true, date: true });
+const Invoice = InvoiceSchema.omit({ id: true, date: true });
 
 export async function createInvoice(formData: FormData) {
-  const path = '/dashboard/invoices'
-  const rawFormData = Object.fromEntries(formData.entries())
+  const path = '/dashboard/invoices';
+  const rawFormData = Object.fromEntries(formData.entries());
 
+  //валидировать данные
+  const { customerId, amount, status } = Invoice.parse(rawFormData);
+  //изменить и добавить данны
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split('T')[0];
+  //добавить данные в БД
+  await sql`INSERT INTO invoices(customer_id, amount, status, date) VALUES(${customerId}, ${amountInCents}, ${status}, ${date})`;
 
-    //валидировать данные
-    const {customerId, amount, status} = CreateInvoice.parse(rawFormData)
-    //изменить и добавить данны
-    const amountInCents = amount * 100;
-    const date = new Date().toISOString().split('T')[0]
-    //добавить данные в БД
-    await sql`INSERT INTO invoices(customer_id, amount, status, date) VALUES(${customerId}, ${amountInCents}, ${status}, ${date})`
+  revalidatePath(path);
+  redirect(path);
+}
 
-    revalidatePath(path);
-    redirect(path)
+export async function updateInvoice(id: string, formData: FormData) {
+  const path = '/dashboard/invoices';
+  const rawFormData = Object.fromEntries(formData.entries());
 
- 
+  const { customerId, amount, status } = Invoice.parse(rawFormData);
+  const amountInCents = amount * 100;
+
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}`;
+
+  revalidatePath(path);
+  // revalidatePath(`/dashboard/invoices/${id}`);
+  redirect(path);
+}
+
+export async function deleteInvoice(id: string) {
+  const path = '/dashboard/invoices';
+
+  await sql`
+    DELETE FROM invoices
+    WHERE id = ${id}`;
+
+  revalidatePath(path);
+  // revalidatePath(`/dashboard/invoices/${id}`);
 }
