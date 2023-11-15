210ba8a901844051ea3edcabf87b23d2f74bceb6
GenIl <topgear09@yandex.ru>
2023-11-15 12:19:46 +0300

    feat: change delay in fetchLatestInvoices

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -42,7 +42,6 @@ export async function fetchRevenue() {
 export async function fetchLatestInvoices() {
   noStore();
   try {
-    await new Promise((resolve) => setTimeout(resolve, 10000));
     //получение последних пяти выставленных счетов + данные о клиенте
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
